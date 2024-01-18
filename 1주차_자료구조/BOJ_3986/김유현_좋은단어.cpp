6762b6abd47ebdc09458e41ca92ff758f1eabe4f
youhyeoneee <khukyh2018@khu.ac.kr>
2024-01-19 08:53:52 +0900

    feat: 김유현_좋은단어

--- 1주차_자료구조/BOJ_3986/김유현_좋은단어.cpp
+++ 1주차_자료구조/BOJ_3986/김유현_좋은단어.cpp
@@ -1,10 +1,65 @@
 #include <iostream>
+#include <stack>
+#include <string>
 
 using namespace std;
 
-int main() {
+int N;
+string str;
+int result = 0;
+
+void fastIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solve(string str) 
+{
+    stack<char> s;
+
+    for(char ch : str) 
+    {
+        if (!s.empty() && s.top() == ch) 
+        {
+            s.pop();
+        }
+        else 
+        {
+            s.push(ch);
+        }      
+    }
     
-    cout << "test";
+    if (s.empty()) 
+    {
+        result++;
+    }
     
+    return;
+}
+
+void output(string str)
+{
+    cout << result << '\n';
+}
+
+void input() 
+{
+    cin >> N;
+    for(int i=0; i<N; i++)
+    {
+        cin >> str;
+        solve(str);
+    }
+
+    output(str);
+}
+
+int main() 
+{
+    fastIO();
+    input();
+
     return 0;
 }
